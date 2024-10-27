097b63503370720fc0a052544ecba055c8109d88
jpsagar <jpsagarm95@gmail.com>
2024-10-27 16:28:25 -0700

    Add the apply rotary embeddings functions

--- model.py
+++ model.py
@@ -43,6 +43,19 @@ def precompute_theta_pos_frequencies(head_dim: int, seq_len: int, device: str, t
     freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
     return freqs_complex
 
+def apply_rotary_embeddings(x: torch.Tensor, freqs_complex: torch.Tensor, device: str):
+    # Shape: (B, seq_len, h, head_dim) -> (B, seq_len, h, head_dim / 2)
+    x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
+    # Shape: (seq_len, head_dim / 2) -> (1, seq_len, 1, head_dim / 2)
+    freqs_complex = freqs_complex.unsqueeze(0).unsqueeze(2)
+    # Shape: (B, seq_len, h, head_dim / 2) * (1, seq_len, 1, head_dim / 2) -> (B, seq_len, h, head_dim / 2)
+    x_rotated = x_complex * freqs_complex
+    # Shape: (B, seq_len, h, head_dim / 2) -> (B, seq_len, h, head_dim / 2, 2)
+    x_out = torch.view_as_real(x_rotated)
+    # Shape: (B, seq_len, h, head_dim / 2, 2) -> (B, seq_len, h, head_dim)
+    x_out = x_out.reshape(*x.shape())
+    return x_out.type_as(x).to(device)
+
 class Transformer(nn.Module):
     def __init__(self, args: ModelArgs) -> None:
         super().__init__()

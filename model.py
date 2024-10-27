8e220733f6e5e001e39bc46eddaa7235d5b04cee
jpsagar <jpsagarm95@gmail.com>
2024-10-26 18:08:00 -0700

    Add the llama2 model arguments

---
+++ model.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class ModelArgs:
+    dim: int = 4096
+    n_layers: int = 32
+    n_heads: int = 32 # Number of heads for the queries
+    n_kv_heads: Optional[int] = None # Number of heads for the K and V
+    vocab_size: int = -1 # This will be set when we load tokenizer
+    multipler_of: int = 256
+    ffn_dim_multiplier: Optional[float] = None
+    norm_eps: float = 1e-5
+
+    # Needed for KV cache
+    max_batch_size: int = 32
+    max_seq_len: int = 2048
+
+    device: str = None

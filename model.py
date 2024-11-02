f88e94d68b5ee4eb9b4f02002c1c5ffe851b50d8
jpsagar <jpsagarm95@gmail.com>
2024-11-02 15:09:49 -0700

    Add feed forward layer code

--- model.py
+++ model.py
@@ -158,6 +158,28 @@ class SelfAttention(nn.Module):
         # Shape: (B, seq_len, dim) -> (B, seq_len, dim)
         return self.wo(output)
 
+class FeedForward(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        hidden_dim = 4 * args.dim
+        hidden_dim = int(2 * hidden_dim / 3)
+        if args.ffn_dim_multiplier is not None:
+            hidden_dim = int(args.ffn_dim_multiplier * hidden_dim)
+        # Round the hidden_dim to the nearest multiple of the multiple_of parameter
+        hidden_dim = args.multipler_of * ((hidden_dim + args.multipler_of - 1) // args.multipler_of)
+        
+        self.w1 = nn.Linear(args.dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, args.dim, bias=False)
+        self.w3 = nn.Linear(args.dim, hidden_dim, bias=False)
+
+    def forward(self, x: torch.Tensor):
+        swish = F.silu(self.w1(x))
+        x_V = self.w3(x)
+        x = swish * x_V
+        x = self.w2(x)
+        return x
+
 class EncoderBlock(nn.Module):
     def __init__(self, args: ModelArgs):
         super().__init__()
